cb79f6ffde15ce9533abf4cdd3f9474d186fa0af
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2021-08-16 10:14:18 -0500

    Add files via upload

--- Kwitter_room.js
+++ Kwitter_room.js
@@ -1,17 +1,44 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBPmBqmNldy5Xuq8yGluAMLFWSWnYZg_Po",
-    authDomain: "kwitter-c479f.firebaseapp.com",
-    projectId: "kwitter-c479f",
-    storageBucket: "kwitter-c479f.appspot.com",
-    messagingSenderId: "419459742875",
-    appId: "1:419459742875:web:9c19421598396ea6195a3e"
-  };
+  apiKey: "AIzaSyCSR2MxCw6d_buv8JwFAFK0AJLDgP7z7bM",
+  authDomain: "kwitter-project-edd5f.firebaseapp.com",
+  databaseURL: "https://kwitter-project-edd5f-default-rtdb.firebaseio.com",
+  projectId: "kwitter-project-edd5f",
+  storageBucket: "kwitter-project-edd5f.appspot.com",
+  messagingSenderId: "154367197245",
+  appId: "1:154367197245:web:95abd4e49672ccd444aa54"
+};
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 
+function addRoom(){
+      room_name = document.getElementById("room_name").value;
 
+      firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room name"    
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+}
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-     Room_names = childKey;
-    
+       Room_names = childKey;   
 
-    
-    });});}
+      
+     console.log("Room Name - " + Room_names);
+     row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#" +Room_names +"</div><hr>";
+     document.getElementById("output").innerHTML += row;
+      
+      
+      });});}
 getData();
+
+function redirectToRoomName(name){
+      console.log(name);
+      localStorage.setItem("room_name" , name);
+      window.location = "kwitter_page.html";
+}
+
+
+
+
